9aa98e18cab5012623a90bcc8b8478936da0866d
Pasindu Lakshan <pasindulakshanx@gmail.com>
2023-02-01 18:19:33 +0530

    Update configJson.ts

--- frontend/src/api/configJson.ts
+++ frontend/src/api/configJson.ts
@@ -2,14 +2,8 @@ let user = JSON.parse(localStorage.getItem("user") || "{}");
 
 if (localStorage.getItem("role") === "admin") {
   user = JSON.parse(localStorage.getItem("admin") || "{}");
-} else if (localStorage.getItem("role") === "instructor") {
-  user = JSON.parse(localStorage.getItem("instructor") || "{}");
-} else if (localStorage.getItem("role") === "student") {
-  user = JSON.parse(localStorage.getItem("student") || "{}");
-} else if (localStorage.getItem("role") === "parent") {
-  user = JSON.parse(localStorage.getItem("parent") || "{}");
-} else if (localStorage.getItem("role") === "teacher") {
-  user = JSON.parse(localStorage.getItem("teacher") || "{}");
+} else if (localStorage.getItem("role") === "user") {
+  user = JSON.parse(localStorage.getItem("user") || "{}");
 }
 
 const requestConfigJson = {

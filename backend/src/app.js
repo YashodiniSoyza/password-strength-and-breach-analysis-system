9ce8979e42ffceeb9a78aaa74247962c84a8d64d
Yashodini Soysa <yasodinisoysa@gmail.com>
2024-09-24 01:33:02 +0530

    Add helmet middleware for enhanced HTTP header security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import logger from "./utils/logger";
 import "dotenv/config";
 import routes from "./api/routes";
@@ -20,6 +21,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 // Inject Response Handler
 app.use((req, res, next) => {
